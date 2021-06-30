cef357a22445ef7f307dd72b9e535fd5275e2f75
tienkim9920 <tienkim9920@gmail.com>
2021-06-30 22:30:27 +0700

    Nou

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,20 @@ import { StoreModule } from '@ngrx/store';
 import { showmenuReducer } from './redux/showmenu.reducer'
 
 import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
+const config = {
+  apiKey: "AIzaSyBpKGIKcJPski1Rg_c0LvE_qQz0asBqVxA",
+  authDomain: "todo-app-tienkim.firebaseapp.com",
+  databaseURL: "https://todo-app-tienkim.firebaseio.com",
+  projectId: "todo-app-tienkim",
+  storageBucket: "todo-app-tienkim.appspot.com",
+  messagingSenderId: "513400048747",
+  appId: "1:513400048747:web:b062ff959830acf0ef752e",
+  measurementId: "G-X1ZCS5H953"
+}
 
 @NgModule({
   declarations: [
@@ -24,8 +35,10 @@ import { environment } from '../environments/environment';
     [BrowserModule, StoreModule.forRoot({ statusMenu: showmenuReducer })],
     AppRoutingModule,
     FormsModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyBpKGIKcJPski1Rg_c0LvE_qQz0asBqVxA",
-    authDomain: "todo-app-tienkim.firebaseapp.com",
-    databaseURL: "https://todo-app-tienkim.firebaseio.com",
-    projectId: "todo-app-tienkim",
-    storageBucket: "todo-app-tienkim.appspot.com",
-    messagingSenderId: "513400048747",
-    appId: "1:513400048747:web:b062ff959830acf0ef752e",
-    measurementId: "G-X1ZCS5H953"
-  }
 };
 
 /*
